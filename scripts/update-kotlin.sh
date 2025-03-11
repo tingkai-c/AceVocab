bdf04f4bcfee965393475ee364957108f9b2dd62
Ting Kai Chiu <60634933+tingkai-c@users.noreply.github.com>
2025-03-11 13:27:45 +0800

    Update update-kotlin.sh

--- scripts/update-kotlin.sh
+++ scripts/update-kotlin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script updates Kotlin version in the Android build files after prebuild
-sed -i 's/ext.kotlin_version = .*/ext.kotlin_version = "1.9.25"/' android/build.gradle
+sed -i 's/ext.kotlin_version = .*/ext.kotlin_version = "1.9.24"/' android/build.gradle
